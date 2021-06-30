452c2b117c2bf8f5456a79d600a721a46896b121
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2021-06-30 22:17:01 +0300

    bugs are fixed

--- week1/GreenGrocer.java
+++ week1/GreenGrocer.java
@@ -3,14 +3,10 @@ package week1;
 import java.util.Scanner;
 
 /**
- * Kullanıcıların manavdan almış oldukları ürünlerin ağırlık değerlerini alıp, 
+ * Kullanıcıların manavdan almış oldukları ürünlerin ağırlık değerlerini alıp,
  * bu değerlere göre toplam tutarı ekrana yazdıran program.
  * 
- * Fiyat listesi:
- * Armut : 2,14 TL
- * Elma : 3,67 TL
- * Domates : 1,11 TL
- * Muz: 0,95 TL
+ * Fiyat listesi: Armut : 2,14 TL Elma : 3,67 TL Domates : 1,11 TL Muz: 0,95 TL
  * Patlıcan : 5,00 TL
  * 
  */
@@ -24,35 +20,32 @@ public class GreenGrocer {
         double total = 0;
         double weight;
         double prices[] = { PEAR, APPLE, TOMATO, BANANA, EGGPLANT };
-        String[] messages = {
-            "Weight of pear in kgs: " ,
-            "Weight of apple in kgs: ",
-            "Weight of tomato in kgs: ",
-            "Weight of banana in kgs: ",
-            "Weight of eggplant in kgs: "
-        };
+        String[] messages = { "Weight of pear in kgs: ", "Weight of apple in kgs: ", "Weight of tomato in kgs: ",
+                "Weight of banana in kgs: ", "Weight of eggplant in kgs: " };
 
+        Scanner scan = new Scanner(System.in);
         for (int i = 0; i < messages.length; i++) {
-            weight = getWeight(messages[i]);
+            weight = getWeight(messages[i], scan);
             total += weight * prices[i];
         }
+        scan.close();
 
         System.out.print("Toplam tutar: " + total);
     }
 
     /**
-     * input: a message to ask the weight value for a particular product
-     * output: weight value of the particular product
+     * input: a message to ask the weight value for a particular product output:
+     * weight value of the particular product
      */
-    public static double getWeight(String message){
+    public static double getWeight(String message, Scanner scan) {
         System.out.print(message);
 
-        Scanner scan = new Scanner(System.in);
-        while(!scan.hasNextDouble()){
-            System.out.print("Weight value must be a number!");
+        while (!scan.hasNextDouble()) {
+            System.out.println("Weight value must be a number");
+            System.out.print(message);
+            scan.next();
         }
-        scan.close();
 
         return scan.nextDouble();
-    }   
+    }
 }
