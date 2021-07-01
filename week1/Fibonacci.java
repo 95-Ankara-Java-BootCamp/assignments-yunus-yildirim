1255b9a5125c20aa37384b9e21ca51e0271bed6f
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2021-07-01 11:19:38 +0300

    n inited as random

--- week1/Fibonacci.java
+++ week1/Fibonacci.java
@@ -1,12 +1,17 @@
 package week1;
 
+import java.util.Random;
+
 /**
  * Program that prints first n fibonacci numbers for the given value n
  */
 public class Fibonacci {
     public static void main(String[] args) {
-        int n = 11;
-
+        // Create instance of Random class
+        Random rand = new Random();
+        // Generate a random integer in range 0 to 99
+        int n = rand.nextInt(100);
+        // Print all fibonacci numbers till n 
         for (int i = 0; i < n; i++) {
             System.out.print(fibonacci(i) + " ");
         }
@@ -17,8 +22,10 @@ public class Fibonacci {
      * output: nth fibonacci number
      */
     public static int fibonacci(int n) {
+        // Base condition
         if (n == 0 || n == 1)
             return n;
+        
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
