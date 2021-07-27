38024bac45467666cce80287ecc4f8c58f886271
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2021-07-27 08:50:09 +0300

    purchase message changed

--- week3/FlightBookingBase.java
+++ week3/FlightBookingBase.java
@@ -107,13 +107,27 @@ public abstract class FlightBookingBase {
     }
 
     public boolean pickASeat(int selectedSeat) {
+        String purchasedSeat = getOrdinal(selectedSeat + 1);
         if (seats[selectedSeat] == false) {
             seats[selectedSeat] = true;
-            System.out.println("The selected seat has been purchased. Have a nice trip!");
+            System.out.println(purchasedSeat + " seat has been purchased. Have a nice trip!");
             return false;
         } else {
-            System.out.println("The selected seat is taken.");
+            System.out.println(purchasedSeat + " seat is taken.");
             return true;
+        }
+    }
+
+    public static String getOrdinal(int i) {
+        String[] suffixes = new String[] { "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th" };
+        switch (i % 100) {
+            case 11:
+            case 12:
+            case 13:
+                return i + "th";
+            default:
+                return i + suffixes[i % 10];
+
         }
     }
 
